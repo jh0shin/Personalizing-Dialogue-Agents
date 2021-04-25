2e6ca115d03f210393c55ad69989412b4f7be47d
junho shin <jh0shin2004@naver.com>
2021-04-26 00:54:24 +0900

    Add kvmemnn hits@1 0.4679 and example

--- display.py
+++ display.py
@@ -1,12 +1,13 @@
 import sys
 from parlai.scripts.display_model import DisplayModel
 
-f = open("seq2seq_self_1000.txt", 'w')
+f = open("kv_self_short_0.4679.txt", 'w')
 sys.stdout = f
 
 DisplayModel.main(
     task='personachat',
-    model_file='model/seq2seq/self_persona_2',
+    # model_file='model/seq2seq/self_persona_2',
+    model_file='model/kvmemnn/self_persona_short',
     num_examples=1000,
 )
 
--- eval.py
+++ eval.py
@@ -4,10 +4,17 @@ from parlai.scripts.eval_model import EvalModel
 
 EvalModel.main(
     task='personachat',
-    model_file='model/seq2seq/self_persona_2',
 
-    batchsize=512,
-    metrics='hits@1',
-    datatype='test',
+    ##### generative model - seq2seq #####
+    # model_file='model/seq2seq/self_persona_2',
+    # batchsize=512,
+    # # metrics='hits@1',
+    # datatype='test',
     # rank_candidates=True, 
+
+    ##### ranking model - kv memory network #####
+    model_file='model/kvmemnn/self_persona_short',
+    # batchsize=512,
+    metrics='hits@1',
+    datatype='test',    
 )
